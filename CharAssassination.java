cb2bc65ad7020b149483a7bf163219e97723e37e
Travis Thomas Messall <messalltt@s.dcsdk12.org>
2018-08-17 08:11:29 -0600

    Initial commit with all exercises and code to ignore all .class files

---
+++ CharAssassination.java
@@ -0,0 +1,8 @@
+public class CharAssassination
+{
+	public static void main ( String[] args )
+	{
+		char ch = "A" ;
+		System.out.println("A char: " + ch );
+	}
+}
---
+++ DoubleJeopardy.java
@@ -0,0 +1,8 @@
+public class DoubleJeopardy
+{
+	public static void main ( String[] args )
+	{
+		double value = 2000000000000000000;
+		System.out.println("A double: " + value);
+	}
+}
