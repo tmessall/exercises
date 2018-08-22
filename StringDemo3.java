ce3079c6648e1566f1ba754de55e0215a6671f65
Travis Thomas Messall <messalltt@s.dcsdk12.org>
2018-08-22 08:19:53 -0600

    Final chapter 11 exercise, play with substring function

---
+++ StringDemo3.java
@@ -0,0 +1,10 @@
+public class StringDemo3{
+  public static void main ( String[] args ) {
+    
+    String str = new String( "Golf is a good walk spoiled." );
+    String sub = str.substring(8, 19);
+
+    System.out.println( sub );
+
+  }
+}
