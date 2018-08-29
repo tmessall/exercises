f17f1e2741594cc93f08e8cff1dde2c650a13778
tmessall <messalltt@s.dcsdk12.org>
2018-08-29 09:21:18 -0600

    Rename folders using numbers so they're better organized

--- Chapter09/QuadraticValue.java
+++ Chapter09/QuadraticValue.java
@@ -2,7 +2,7 @@ public class QuadraticValue
 {
 	public static void main ( String[] args )
 	{
-		double x = 2/3;
+		double x = 2.0/3;
 		System.out.println("At X = " + x + " the value is " + ( 3 * x * x - 8 * x + 4) );
 	}
 }
