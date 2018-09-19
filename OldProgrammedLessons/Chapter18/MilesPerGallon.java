34a8649bb4a56da3ec5032dd635e2b93e473a4c0
tmessall <messalltt@s.dcsdk12.org>
2018-09-19 08:24:12 -0600

    Complete exercise 2 from old chapter 18, loop practice

---
+++ OldProgrammedLessons/Chapter18/MilesPerGallon.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MilesPerGallon{
+	public static void main(String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		int initialMiles;
+		int finalMiles;
+		int gallons;
+
+		System.out.println("Inital miles: ");
+		initialMiles = scan.nextInt();
+
+		while (initialMiles >= 0){
+			System.out.println("Final miles:");
+			finalMiles = scan.nextInt();
+			System.out.println("Gallons:");
+			gallons = scan.nextInt();
+
+			double milesPerGallon = (finalMiles - initialMiles + 0.0)/gallons;
+
+			System.out.println("Miles per Gallon: " + milesPerGallon);
+			
+			System.out.println("Initial miles:  ");
+			initialMiles = scan.nextInt();
+		}
+
+		System.out.println("bye");
+	}
+}
