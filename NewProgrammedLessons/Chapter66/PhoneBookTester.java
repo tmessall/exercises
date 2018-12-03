1a714ffb96d2cf0f9e5469a3955f41aadc8a5bb3
tmessall <messalltt@s.dcsdk12.org>
2018-12-03 08:18:57 -0700

    Update the README and complete chapter 66 exercise 2

--- NewProgrammedLessons/Chapter66/PhoneBookTester.java
+++ NewProgrammedLessons/Chapter66/PhoneBookTester.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 class PhoneEntry
 {
-  private String name;
+  private String lastName;
+  private String firstName;
   private String phone;
 
-  public PhoneEntry( String n, String p )
+  public PhoneEntry( String m, String n, String p )
   {
-    name = n; phone = p;
+    firstName = m; lastName = n; phone = p;
   }
   
-  public String getName()  {return name;}
+  public String getFirstName()  {return firstName;}
+  public String getLastName() {return lastName;}
   public String getPhone() {return phone;}
 }
 
@@ -20,27 +22,41 @@ class PhoneBook
 
   public PhoneBook()    // constructor
   {
-    phoneBook = new PhoneEntry[ 5 ] ;
+    phoneBook = new PhoneEntry[ 7 ] ;
 
-    phoneBook[0] = new PhoneEntry( "James Barclay", "(418) 665-1223" );
-    phoneBook[1] = new PhoneEntry( "Grace Dunbar", "(860) 399-3044" );
-    phoneBook[2] = new PhoneEntry( "Paul Kratides", "(815) 439-9271" );
-    phoneBook[3] = new PhoneEntry( "Violet Smith", "(312) 223-1937" );
-    phoneBook[4] = new PhoneEntry( "John Wood", "(913) 883-2874" );
+    phoneBook[0] = new PhoneEntry( "James", "Barclay", "(418) 665-1223" );
+    phoneBook[1] = new PhoneEntry( "Grace", "Dunbar", "(860) 399-3044" );
+    phoneBook[2] = new PhoneEntry( "Paul", "Kratides", "(815) 439-9271" );
+    phoneBook[3] = new PhoneEntry( "Violet", "Smith", "(312) 223-1937" );
+    phoneBook[4] = new PhoneEntry( "John", "Wood", "(913) 883-2874" );
+    phoneBook[5] = new PhoneEntry( "John", "Smith", "(812) 339-4916");
+    phoneBook[6] = new PhoneEntry( "Willoughby", "Smith", "(312) 992-8761");
 
   }
 
-  public PhoneEntry search( String targetName )  
+  public PhoneEntry search(String targetFirstName, String targetLastName)  
   {
     for ( int j=0; j < phoneBook.length; j++ )
     {
-      if ( phoneBook[ j] != null && phoneBook[ j ].getName().toUpperCase().equals( targetName ) )
-        return phoneBook[ j ];
+      if ( phoneBook[ j] != null && phoneBook[j].getFirstName().toUpperCase().equals(targetFirstName) && phoneBook[j].getLastName().toUpperCase().equals(targetLastName)) {
+        return phoneBook[j];
+      }
     }
 
     return null;
   }
+
+  public PhoneEntry[] searchAllLasts(String targetName) {
+    PhoneEntry[] allNames = new PhoneEntry[phoneBook.length];
+    for (int j = 0; j < phoneBook.length; j++) {
+      if ( phoneBook[j] != null && phoneBook[j].getLastName().toUpperCase().equals(targetName)) {
+        allNames[j] = phoneBook[j];
+      }
+    }
+    return allNames;
+  }
 }
+
 
 public class PhoneBookTester
 {
@@ -49,18 +65,30 @@ public class PhoneBookTester
     PhoneBook pb = new PhoneBook(); 
     Scanner scan = new Scanner(System.in); 
   
-    System.out.println("Name?");
-    String searchName = scan.nextLine();
+    System.out.println("Last Name?");
+    String searchLastName = scan.nextLine();
 
-    while (! searchName.equals("quit")){
-		PhoneEntry entry = pb.search(searchName.toUpperCase()); 
-   		if ( entry != null )
-   			System.out.println("The number is: " + entry.getPhone());
-  		else
-   			System.out.println("Name not found" );
-   		System.out.println("");
-   		System.out.println("Name?");
-   		searchName = scan.nextLine();
+    while (! searchLastName.equals("quit")){
+      System.out.println("First Name?");
+      String searchFirstName = scan.nextLine();
+      if (searchFirstName.isEmpty()) {
+        PhoneEntry[] entries = pb.searchAllLasts(searchLastName.toUpperCase());
+        for (int i = 0; i < entries.length; i++) {
+          if (entries[i] != null) {
+            System.out.println(entries[i].getFirstName() + " " + entries[i].getLastName() + ": " + entries[i].getPhone());
+          }
+        }
+      } else {
+        PhoneEntry entry = pb.search(searchFirstName.toUpperCase(), searchLastName.toUpperCase()); 
+        if ( entry != null ) {
+         System.out.println("The number is: " + entry.getPhone());
+        } else {
+          System.out.println("Name not found" );
+        }
+      }  
+      System.out.println("");
+      System.out.println("Last Name?");
+      searchLastName = scan.nextLine();
     }
 
     System.out.println("good-by");
